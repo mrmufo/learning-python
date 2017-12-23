79fd5e13a5123b1126a4d836091c464f80a8e9fc
Wojciech Rykala <rykala.wojciech@gmail.com>
2017-12-23 14:34:52 +0000

    Added some if, elif statements

--- if-program-flow/if-program-flow.py
+++ if-program-flow/if-program-flow.py
@@ -11,18 +11,67 @@
 # else:
 #     print("Incorrect data. Age can not be less than 1.")
 #
-print("Please guess a number between 1 and 10: ")
-guess = int(input())
+#
+# print("Please guess a number between 1 and 10: ")
+# guess = int(input())
+#
+# if guess != 5:
+#     if guess < 5:
+#         print("Please, guess higher.")
+#     else:
+#         print("Please, guess lower.")
+#     guess = int(input())
+#     if guess == 5:
+#         print("Well done, you guessed it.")
+#     else:
+#         print("Sorry, you have not guessed correctly.")
+# else:
+#     print("Well done, you guessed it in first try!")
+#
+# age = int(input("How old are you? "))
+#
+# # if (age <= 16) and (age <= 65):
+# if 16 <= age <= 65:
+#     print("Have a good day at work! ")
+#
+# if (age < 16) or (age > 65):
+#     print("Enjoy your free time! ")
+# else:
+#     print("Have a good day at work! ")
+#
+#
+# print("""False: {0}
+# None: {1}
+# 0: {2}
+# 0.0: {3}
+# empty list []: {4}
+# empty tuple (): {5}
+# empty string '': {6}
+# empty string "": {7}
+# empty mapping {{}}: {8}
+# """.format(False, bool(None), bool(0), bool(0.0), bool([]), bool(()), bool(''), bool(""), bool({})))
+#
+# x = input("Please enter some text. ")
+# if x:
+#     print("You entered '{}'".format(x))
+# else:
+#     print("You did not enter anything. ")
+#
+# print(not False)
+# print(not True)
+#
+# age = int(input("How old are you?"))
+# if not (age < 18):
+#     print("You are old enough to vote")
+#     print("Please put X in the box. ")
+# else:
+#     print("Please come back in {0} years. ".format(18 - age))
+#
+parrot = "Norwegian Blue"
+letter = input("Enter a character: ")
 
-if guess != 5:
-    if guess < 5:
-        print("Please, guess higher.")
-    else:
-        print("Please, guess lower.")
-    guess = int(input())
-    if guess == 5:
-        print("Well done, you guessed it.")
-    else:
-        print("Sorry, you have not guessed correctly.")
+if letter in parrot:
+    print("Give me an {}, Bob. ".format(letter))
 else:
-    print("Well done, you guessed it in first try!")
+    print("I don't need that letter. ")
+
