38b8a67be96fcaea538f95a9fc12481d47fa834c
Wojciech Rykala <rykala.wojciech@gmail.com>
2017-12-27 12:54:17 +0000

    Added continue examples git status
    wq
    :wq

---
+++ continue-break-else/continue-break-else.py
@@ -0,0 +1,8 @@
+# shopping_list = ["milk", "pasta", "eggs", "spam", "rice"]
+# for item in shopping_list:
+#     if item == 'spam':
+#         print("I am ignoring " + item)
+#         continue
+#
+#     print("Buy " + item)
+#
