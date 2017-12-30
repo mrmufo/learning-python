a8b19bacc4de2764ba751ca159bc97365797ed81
Wojciech Rykala <rykala.wojciech@gmail.com>
2017-12-30 14:46:30 +0000

    Corrections and code improvement

--- pf-challenge/pf-challenge.py
+++ pf-challenge/pf-challenge.py
@@ -1,15 +1,21 @@
-ipAddress = input("Please enter your IP address: ")
+ipAddress = input("Please enter your IP address. "
+                  "IP address should consist of 4 numbers, separated from each other with a full stop. \n")
+if ipAddress[-1] != '.':
+    ipAddress += '.'
+
 segmentNum = 1
 segmentLen = 0
-for i in range(0, len(ipAddress)):
-    if ipAddress[i] == ".":
+errorsInIP = 0
+for character in ipAddress:  # for i in range(0, len(ipAddress)):
+    if character == '.':        # if ipAddress[i] == ".":
+        if segmentLen == 0 and errorsInIP == 0 and segmentNum == 1:
+            print("NOTE: IP address should not begin with a dot. ")
+            errorsInIP += 1
+            continue
         print("Segment number {0} is {1} characters long. ".format(segmentNum, segmentLen))
         segmentLen = 0
-        if (i + 1) == len(ipAddress):
-            break
         segmentNum += 1
         continue
     segmentLen += 1
-if segmentLen != 0:
-    print("Segment number {0} is {1} characters long. ".format(segmentNum, segmentLen))
-print("Your IP address consists of {0} segments. ".format(segmentNum))
+
+print("Your IP address consists of {0} segments. ".format(segmentNum - 1))
