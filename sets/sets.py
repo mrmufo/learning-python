59dba2833786e598e165cd2ee6e26a1a8e60bf97
Wojciech Rykala <rykala.wojciech@gmail.com>
2018-01-25 21:41:32 +0000

    Started sets

---
+++ sets/sets.py
@@ -0,0 +1,22 @@
+# farmAnimals = {"sheep", "cow", "hen"}
+# print(farmAnimals)
+#
+# for animal in farmAnimals:
+#     print(animal)
+#
+# print("=" * 40)
+#
+# wildAnimals = set(["lion", "tiger", "panther", "elephant"])
+# print(wildAnimals)
+#
+# for animal in wildAnimals:
+#     print(animal)
+#
+# farmAnimals.add("horse")
+# wildAnimals.add("horse")
+# print(farmAnimals)
+# print(wildAnimals)
+
+emptySet = set()
+emptySet2 = {}
+emptySet.add("a")
