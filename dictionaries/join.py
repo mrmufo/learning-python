9bd00adecac31cb0b8e1917e7aca202763999cef
Wojciech Rykala <rykala.wojciech@gmail.com>
2018-01-25 19:30:32 +0000

    Completed challenge, added join & split examples

--- dictionaries/join.py
+++ dictionaries/join.py
@@ -1,7 +1,39 @@
-myList = ["a", "b", "c", "d"]
+# myList = ["a", "b", "c", "d"]
+# letters = "abcdefghijklmnopqrstuvwxyz"
+# numbers = "123456789"
+# separator = ", "
+#
+# newString = separator.join(letters)
+# print(newString)
 
-# newString = ''
-# for c in myList:
-newString = ", ".join(myList)  # += c + ", "
+locations = {0: "You are sitting in front of computer learning Python",
+             1: "You are standing at the end of a road before a small brick building",
+             2: "You are at the top of a hill",
+             3: "You are inside a building, a well house for a small stream",
+             4: "You are in a valley beside a stream",
+             5: "You are in the forest"}
 
-print(newString)
+exits = [{"Q": 0},
+         {"W": 2, "E": 3, "N": 5, "S": 4, "Q": 0},
+         {"N": 5, "Q": 0},
+         {"W": 1, "Q": 0},
+         {"N": 1, "W": 2, "Q": 0},
+         {"W": 2, "S": 1, "Q": 0}]
+
+loc = 1
+while True:
+    availableExits = ", ".join(exits[loc].keys())
+    # for direction in exits[loc].keys():
+    #     availableExits += direction + ", "
+
+    print(locations[loc])
+
+    if loc == 0:
+        break
+
+    direction = input("Available exits are " + availableExits + " ").upper()
+    print()
+    if direction in exits[loc]:
+        loc = exits[loc][direction]
+    else:
+        print("You cannot go in that direction")
