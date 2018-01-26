ecfda3424d1187809b75aa95461d99f9a3b3d39b
Wojciech Rykala <rykala.wojciech@gmail.com>
2018-01-26 20:31:32 +0000

    Started sets, added some examples

--- sets/sets.py
+++ sets/sets.py
@@ -16,7 +16,65 @@
 # wildAnimals.add("horse")
 # print(farmAnimals)
 # print(wildAnimals)
+#
+# emptySet = set()
+# emptySet2 = {}
+# emptySet.add("a")
+#
+# even = set(range(0, 40, 2))
+# print(even)
+# squares_tuple = (4, 6, 9, 16, 25)
+# squares = set(squares_tuple)
+# print(squares)
+# even = set(range(0, 40, 2))
+# print(even)
+# print(len(even))
+#
+# squares_tuple = (4, 6, 9, 16, 25)
+# squares = set(squares_tuple)
+# print(squares)
+# print(len(squares))
+#
+# print(even.union(squares))
+# print(len(even.union(squares)))
+#
+# print(squares.union(even))
+#
+# print("-" *40)
+#
+# print(even.intersection(squares))
+# print(even & squares)
+# print(squares.intersection(even))
+# print(squares & even)
+even = set(range(0, 40, 2))
+print(even)
+squares_tuple = (4, 6, 9, 16, 25)
+squares = set(squares_tuple)
+print(sorted(squares))
 
-emptySet = set()
-emptySet2 = {}
-emptySet.add("a")
+# print("even minus squares")
+# print(sorted(even.difference(squares)))
+# print(sorted(even - squares))
+#
+# print("sqares minus even")
+# print(squares.difference(even))
+# print(squares - even)
+#
+# print("=" * 40)
+# print(sorted(even))
+# print(squares)
+# even.difference_update(squares)
+# print(sorted(even))
+
+# print("symmetric even minus squares")
+# print(sorted(even.symmetric_difference(squares)))
+#
+# print("symmetric squares minus even")
+# print(sorted(squares.symmetric_difference(even)))
+
+squares.discard(4)
+squares.discard(16)
+squares.discard(8)
+print(squares)
+
+squares.remove(8)
