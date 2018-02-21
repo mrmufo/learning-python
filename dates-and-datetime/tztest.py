2d603801143697a5a4ca08a5f55d6b1a3110543b
Wojciech Rykala <rykala.wojciech@gmail.com>
2018-02-21 18:09:25 +0000

    Added more examples using pytz

--- dates-and-datetime/tztest.py
+++ dates-and-datetime/tztest.py
@@ -5,4 +5,24 @@ country = 'Europe/Moscow'
 tz_to_display = pytz.timezone(country)
 local_time = datetime.datetime.now(tz=tz_to_display)
 print("The time in {country} is {time}".format(country=country, time=local_time))
-print("UTC is {UTC}".format(datetime.datetime.utcnow()))
+print("UTC is {UTC}".format(UTC=datetime.datetime.utcnow()))
+
+# for x in pytz.all_timezones:
+#     print(x)
+#
+# for x in sorted(pytz.country_names):
+#     print(x + ": " + pytz.country_names[x])
+#
+# for x in sorted(pytz.country_names):
+#     print("{}: {}: {}".format(x, pytz.country_names[x], pytz.country_timezones.get(x)))
+#
+for x in sorted(pytz.country_names):
+    print("{}: {}".format(x, pytz.country_names[x], end=': '))
+    if x in pytz.country_timezones:
+        for zone in sorted(pytz.country_timezones[x]):
+            tz_to_display = pytz.timezone(zone)
+            local_time = datetime.datetime.now(tz=tz_to_display)
+            print("\t\t{}: {}".format(zone, local_time))
+    else:
+        print("\t\tNo time zone defined")
+
