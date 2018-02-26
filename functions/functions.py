8d8787274ca70c90204b4d363ce8c31460552503
Wojciech Rykala <rykala.wojciech@gmail.com>
2018-02-26 19:16:24 +0000

    Added function examples

--- functions/functions.py
+++ functions/functions.py
@@ -5,14 +5,24 @@ def python_food():
     print(" " * left_margin, text)
 
 
-def centre_text(text):
+def centre_text(*args, sep=' '):  # , end='\n', file=None, flush=False):
+    text = ""
+    for arg in args:
+        text += str(arg) + sep
     left_margin = (80 - len(text)) // 2
-    print(" " * left_margin, text)
+    return " " * left_margin + text
+    #  print(" " * left_margin, text, end=end, file=file, flush=flush)
 
+
+# with open("centred", mode='w') as centred_file:
 
 # Call the function
-centre_text("spam and eggs")
-centre_text("spam, spam and eggs")
-centre_text("spam, spam, spam and spam")
+s1 = centre_text("spam and eggs")
+print(s1)
+print(centre_text("spam, spam and eggs"))
+print(centre_text("spam, spam, spam and spam"))
+print(centre_text(12))
+print(centre_text("first", "second", 3, 4, "spam", sep=":"))
 
-print("first", "second", 3, 4, "spam")
+print("=" + str(12 * 3))
+print(sorted(["b", "d", "c", "a"]))
