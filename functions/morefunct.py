0061c937d23fad767831ad1dd4c5c688c93103c8
mrmufo <rykala.wojciech@gmail.com>
2018-02-28 17:48:15 +0000

    Added some graphs using functions

--- functions/morefunct.py
+++ functions/morefunct.py
@@ -1,26 +1,44 @@
+import math
 try:
     import tkinter
 except ImportError:  # python 2
     import Tkinter as tkinter
 
 
-def parabola(x):
-    y = x * x/10
-    return y
+def parabola(c, size):
+    for x in range(size):
+        y = x * x / size
+        plot(c, x, y)
+        plot(c, -x, y)
 
 
-def draw_axes(canvas):
-    canvas.update()
-    x_origin = canvas.winfo_width() / 2
-    y_origin = canvas.winfo_height() / 2
-    canvas.configure(scrollregion=(-x_origin, -y_origin, x_origin, y_origin))
-    canvas.create_line(-x_origin, 0, x_origin, 0, fill="black")
-    canvas.create_line(0, y_origin, 0, -y_origin, fill="black")
+# Modify the circle function that it allows the colour of the circle to be specified
+# and defaults to red if a colour is not given. You may want to review the previous lectures
+# about named parameters and default values.
+#
+def circle(c, radius, g, h, colour="red"):
+    c.create_oval(g + radius, h + radius, g - radius, h - radius, outline=colour, width=2)
+    # for x in range(g * 10, (g + radius) * 10):
+    #     x /= 10
+    #     y = h + (math.sqrt(radius ** 2 - ((x-g) ** 2)))
+    #     plot(c, x, y)
+    #     plot(c, x, 2 * h - y)
+    #     plot(c, 2 * g - x, y)
+    #     plot(c, 2 * g - x, 2 * h - y)
+
+
+def draw_axes(c):
+    c.update()
+    x_origin = c.winfo_width() / 2
+    y_origin = c.winfo_height() / 2
+    c.configure(scrollregion=(-x_origin, -y_origin, x_origin, y_origin))
+    c.create_line(-x_origin, 0, x_origin, 0, fill="black")
+    c.create_line(0, y_origin, 0, -y_origin, fill="black")
     print(locals())
 
 
-def plot(canvas, x, y):
-    canvas.create_line(x, y, x + 1, y + 1, fill="red")
+def plot(c, x, y):
+    c.create_line(x, -y, x + 1, -y + 1, fill="red")
 
 
 mainWindow = tkinter.Tk()
@@ -28,18 +46,24 @@ mainWindow = tkinter.Tk()
 mainWindow.title("Parabola")
 mainWindow.geometry("640x480")
 
-canvas = tkinter.Canvas(mainWindow, width=320, heigh=480)
+canvas = tkinter.Canvas(mainWindow, width=640, heigh=480)
 canvas.grid(row=0, column=0)
 
-canvas2 = tkinter.Canvas(mainWindow, width=320, height=480, background="blue")
-canvas2.grid(row=0, column=1)
-
-print(repr(canvas), repr(canvas2))
+# print(repr(canvas), repr(canvas2))
 draw_axes(canvas)
-draw_axes(canvas2)
 
-for x in range(-100, 100):
-    y = parabola(x)
-    plot(canvas, x, -y)
-
+# for x in range(-100, 100):
+#     y = parabola(x)
+#     plot(canvas, x, -y)
+parabola(canvas, 100)
+parabola(canvas, 200)
+circle(canvas, 100, 100, 100, "yellow")
+circle(canvas, 100, 100, -100)
+circle(canvas, 100, -100, 100)
+circle(canvas, 100, -100, -100, "blue")
+circle(canvas, 10, 30, 30)
+circle(canvas, 10, 30, -30)
+circle(canvas, 10, -30, 30)
+circle(canvas, 10, -30, -30)
+circle(canvas, 30, 0, 0, "green")
 mainWindow.mainloop()
