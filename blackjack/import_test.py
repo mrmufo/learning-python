b830c8d571d597970a660a2196f5f85609cc715b
mrmufo <rykala.wojciech@gmail.com>
2018-03-11 20:58:32 +0000

    Modified Blackjack game

--- blackjack/import_test.py
+++ blackjack/import_test.py
@@ -1,4 +1,15 @@
 import blackjack
 
+# g = sorted(globals())
+# for x in g:
+#     print(x)
+
 print(__name__)
 blackjack.play()
+print(blackjack.cards)
+
+# personal_details = ("Wojtek", 24, "Poland")
+#
+# name, _, country = personal_details
+# print(name, country)
+# print(_)
