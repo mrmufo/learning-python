8bd5c7c4ad21d14d60313a0321a4089f1dea1384
mrmufo <rykala.wojciech@gmail.com>
2018-03-23 14:07:30 +0000

    Started with OOP, example added

---
+++ oop/oop.py
@@ -0,0 +1,57 @@
+class Kettle(object):
+
+    power_source = "electricity"
+
+    def __init__(self, make, price):
+        self.make = make
+        self.price = price
+        self.on = False
+
+    def switch_on(self):
+        self.on = True
+
+
+kenwood = Kettle("Kenwood", 8.99)
+print(kenwood.make)
+print(kenwood.price)
+
+kenwood.price = 12.75
+print(kenwood.price)
+
+hamilton = Kettle("Hamilton", 14.55)
+
+print("Models: {make1} = {price1}, {make2} = {price2}".format(
+    make1=kenwood.make, price1=kenwood.price, make2=hamilton.make, price2=hamilton.price))
+
+print("Models: {0.make} = {0.price}, {1.make} = {1.price}".format(kenwood, hamilton))
+
+""" Class: template for creating objects.
+    Object: an instance of a class.
+    Instantiate: create an instance of a class.
+    Method: a function defined in a class.
+    Attribute: a variable bound to and instance of a class. """
+
+print(hamilton.on)
+hamilton.switch_on()
+print(hamilton.on)
+
+Kettle.switch_on(kenwood)
+print(kenwood)
+kenwood.switch_on()
+
+print("*" * 80)
+
+kenwood.power = 1.5
+print(kenwood.power)
+# print(hamilton.power)
+
+print("Switch to atomic power")
+Kettle.power_source = "atomic"
+print(Kettle.power_source)
+print("Switch kenwood to gas")
+kenwood.power_source = "gas"
+print(kenwood.power_source)
+print(hamilton.power_source)
+print(Kettle.__dict__)
+print(kenwood.__dict__)
+print(hamilton.__dict__)
