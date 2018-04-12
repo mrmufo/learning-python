86d984f261e22dbb3c52c257554d0c5da56db99b
mrmufo <rykala.wojciech@gmail.com>
2018-04-12 16:31:19 +0100

    Added getters and setters

---
+++ game/main.py
@@ -0,0 +1,17 @@
+from player import Player
+
+woj = Player("Woj")
+
+print(woj.name)
+print(woj.lives)
+woj.lives -= 1
+print(woj)
+
+woj.lives -= 1
+print(woj)
+
+woj.lives -= 1
+print(woj)
+
+woj.lives -= 1
+print(woj)
