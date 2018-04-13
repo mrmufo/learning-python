4dcee1940aefeb7274a99f8aa2fbf6c0eaeecfe9
mrmufo <rykala.wojciech@gmail.com>
2018-04-13 20:09:03 +0100

    Added subclass, grunt method and monster dying

--- game/enemy.py
+++ game/enemy.py
@@ -4,6 +4,7 @@ class Enemy:
         self.name = name
         self.hit_points = hit_points
         self.lives = lives
+        self.alive = True
 
     def take_damage(self, damage):
         remaining_points = self.hit_points - damage
@@ -12,7 +13,11 @@ class Enemy:
             print("I took {dmg} points damage and have {hp} hp left".format(dmg=damage, hp=self.hit_points))
         else:
             self.lives -= 1
-            print("Live lost")
+            if self.lives > 0:
+                print("{0.name} lost a life.".format(self))
+            else:
+                print("{0.name} is dead.".format(self))
+                self.alive = False
 
     def __str__(self):
         return "Name: {0.name}, Lives: {0.lives}, Hit points: {0.hit_points}".format(self)
@@ -27,3 +32,8 @@ class Troll(Enemy):
 
     def grunt(self):
         print("Me {0.name}. {0.name} stomp you.".format(self))
+
+
+class Vampire(Enemy):
+    def __init__(self, name):
+        super().__init__(name=name, lives=3, hit_points=12)
--- game/main.py
+++ game/main.py
@@ -1,4 +1,4 @@
-from enemy import Enemy, Troll
+from enemy import Enemy, Troll, Vampire
 
 ugly_troll = Troll("Pug")
 print("Ugly troll - {}".format(ugly_troll))
@@ -12,3 +12,16 @@ print(brother)
 ugly_troll.grunt()
 another_troll.grunt()
 brother.grunt()
+
+awaken_vampire = Vampire("Sleepy")
+print(awaken_vampire)
+awaken_vampire.take_damage(10)
+print(awaken_vampire)
+awaken_vampire.take_damage(5)
+print(awaken_vampire)
+ugly_troll.take_damage(25)
+print(ugly_troll)
+
+while awaken_vampire.alive:
+    awaken_vampire.take_damage(1)
+    print(awaken_vampire)
