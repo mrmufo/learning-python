0f7e0da389ac1a4a4568fb98b525b76b2d1d84e9
mrmufo <rykala.wojciech@gmail.com>
2018-04-13 17:25:21 +0100

    Added subclass, used pass statement

--- game/main.py
+++ game/main.py
@@ -1,13 +1,10 @@
-from enemy import Enemy
+from enemy import Enemy, Troll
 
-random_monster = Enemy("Basic enemy", 12, 1)
-print(random_monster)
+ugly_troll = Troll()
+print("Ugly troll - {}".format(ugly_troll))
 
-random_monster.take_damage(4)
-print(random_monster)
+another_troll = Troll("Ug", 18, 1)
+print("Another troll - {}".format(another_troll))
 
-random_monster.take_damage(9)
-print(random_monster)
-
-random_monster.take_damage(0)
-print(random_monster)
+brother = Troll("Urg", 23)
+print(brother)
